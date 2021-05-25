7c00e388de8a77f29a7f891e6400e82d4fd9d98c
y200-anis <73964093+y200-anis@users.noreply.github.com>
2021-05-25 20:15:50 +0200

    Create Couleur.java

---
+++ Couleur.java
@@ -0,0 +1,12 @@
+package echec;
+/**
+ * @author David Sidoun
+ */
+public enum Couleur {
+	BLACK ("BLACK"),
+	WHITE ("WHITE");
+	Couleur(String s){description = s;}
+	private String description;
+	public String getDescription() {return description;}
+
+}
