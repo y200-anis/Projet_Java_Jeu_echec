2e517d7048010b07017becad763c6ee14285f836
y200-anis <73964093+y200-anis@users.noreply.github.com>
2021-06-05 16:28:27 +0200

    Create Executeur.java

---
+++ Executeur.java
@@ -0,0 +1,10 @@
+package echec;
+/**
+ * @ Ait Taouit Yanis
+ */
+public class Executeur {
+	public static void main(String[] args) {
+		Partie p = new Partie("Joueur blanc","Joueur noir");
+		p.lancer();
+	}
+}
